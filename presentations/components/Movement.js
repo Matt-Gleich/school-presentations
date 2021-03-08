8bb49f475bf68ec9826302e97665cf0d524d7147
Matthew Gleich <git@mattglei.ch>
2021-03-08 16:45:23 -0500

    ✨ Add movement toast
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

---
+++ presentations/components/Movement.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import toast, { Toaster } from 'react-hot-toast'
+import './styles/Movement.css'
+
+let sent = false
+
+export default () => {
+  if (!sent) {
+    toast('Use arrow keys to navigate through slides ← →')
+    sent = true
+  }
+  return <Toaster toastOptions={{ className: 'movement' }} />
+}
