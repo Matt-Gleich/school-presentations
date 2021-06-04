8156122dbc9c9d37228ef76246ce0aea82e284be
Matthew Gleich <git@mattglei.ch>
2021-06-04 00:17:27 -0400

    chore: replace all instances of "Matt-Gleich" with "gleich"

--- presentations/components/Copyright.js
+++ presentations/components/Copyright.js
@@ -6,7 +6,7 @@ export default () => (
   <p className="copyright animate__animated animate__slideInUp">
     &copy; Matthew Gleich {new Date().getFullYear()} | See the{' '}
     <a
-      href="https://github.com/Matt-Gleich/school-presentations"
+      href="https://github.com/gleich/school-presentations"
       target="_blank"
       rel="noreferrer"
     >
