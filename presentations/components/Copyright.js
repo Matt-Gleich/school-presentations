664b8abd11dc44d245755755976f7d4f6980d5bd
Matt Gleich <git@mattglei.ch>
2021-09-07 00:31:35 -0400

    fix: prettier format
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- presentations/components/Copyright.js
+++ presentations/components/Copyright.js
@@ -5,11 +5,7 @@ import 'animate.css'
 export default () => (
   <p className="copyright animate__animated animate__slideInUp">
     &copy; Matt Gleich {new Date().getFullYear()} | See the{' '}
-    <a
-      href="https://github.com/gleich/school"
-      target="_blank"
-      rel="noreferrer"
-    >
+    <a href="https://github.com/gleich/school" target="_blank" rel="noreferrer">
       source code
     </a>
   </p>
